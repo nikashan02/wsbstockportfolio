98ecb12400d32672c48789ce24c0bfa0b015420d
nikashan02 <nikashan02@gmail.com>
2021-01-02 16:22:40 -0500

    Updated Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,21 @@
-import React from "react";
-import './App.css';
+import React, { Component } from 'react';
 import GetData from './getData';
+import { Nav, Navbar, NavDropdown, Form, Button, FormControl } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <GetData></GetData>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+      
+        <br></br>
+        <div className="App" style={{width: "70%", position: 'absolute', left: '50%', transform: 'translate(-50%, 0%)'}} >
+          <GetData />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- frontend/src/getData.js
+++ frontend/src/getData.js
@@ -1,28 +1,99 @@
 import React, { Component } from 'react';
+import BootstrapTable from 'react-bootstrap-table-next';
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
 import axios from 'axios';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const columns = [{
+  dataField: 'stock',
+  text: 'Stock',
+  sort: true,
+  //filter: textFilter()
+}, {
+  dataField: 'shares',
+  text: 'Shares',
+  sort: true
+}, {
+  dataField: 'sentiment',
+  text: 'Sentiment',
+  sort: true
+}, {
+  dataField: 'avg_cost',
+  text: 'Average Cost',
+  sort: true
+}, {
+  dataField: 'curr_price',
+  text: 'Current Price',
+  sort: true
+}, {
+  dataField: 'total_return',
+  text: 'Total Return',
+  sort: true,
+  style: (cell, row, rowIndex, colIndex) => {
+    if (cell.charAt(0) === '+') {
+      return {
+        color: '#4CAF50',
+        fontWeight: 'bold'
+      };
+    } else if (cell.charAt(0) === '0') {
+      return {
+        backgroundColor: '#FFFFFF',
+        fontWeight: 'bold'
+      }
+    }
+    return {
+      color: '#EF5350',
+      fontWeight: 'bold'
+    };
+  }
+},];
+
+const defaultSorted = [{
+  dataField: 'shares',
+  order: 'desc'
+}];
+
 
 export default class GetData extends Component {
   constructor() {
     super();
     this.state = {
-      data: "Not yet gotten"
+      stocks: [
+                {
+                    "stock": null,
+                    "shares": null,
+                    "sentiment": null,
+                    "avg_cost": null,
+                    "total_return": '0',
+                    "curr_price": null
+                }
+              ],
+      width: 400
     };
   }
 
   componentDidMount = () => {
     axios.get("/getPosts").then(response => {
-      console.log(response.data);
       this.setState ({
-        data: response.data
+        stocks: response.data.sort((a,b) => a.shares + b.shares).slice(0,30)
       });
     });
   };
 
   render() {
     return (
-      <div>
-        <h1>Data: {this.state.data}</h1>
-      </div>
+      <BootstrapTable
+        bootstrap4
+        keyField="stock"
+        pagination={ paginationFactory() }
+        data={ this.state.stocks }
+        columns={ columns }
+        defaultSorted={ defaultSorted }
+        filter={ filterFactory() }
+      />
     )
   }
 }
