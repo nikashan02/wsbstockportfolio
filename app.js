18a55b97dfb01774d98b7adbb7ae06fc6d72e42a
nikashan02 <nikashan02@gmail.com>
2021-01-03 20:13:36 -0500

    Fixed init tickers

--- app.js
+++ app.js
@@ -9,7 +9,25 @@ app.use(cors());
 app.use(express.static(path.join(__dirname, 'frontend/build')));
 
 
-let responseTickerList = [];
+let responseTickerList = {
+                          "res": [{
+                                    "stock": "",
+                                    "shares": 0,
+                                    "sentiment": 0,
+                                    "avg_cost": "$0",
+                                    "total_return": "0%",
+                                    "curr_price": "",
+                                    "posts": [
+                                              {
+                                                  "title": "",
+                                                  "upvotes": 0,
+                                                  "url": "",
+                                                  "created": "",
+                                                  "historical": 0
+                                              }
+                                            ]
+                                  }]
+                        };
 
 const { fork } = require('child_process');
 const forked = fork('child.js');
